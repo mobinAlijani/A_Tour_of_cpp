a98f64ab836720b4c44849ce9c4b3ae869b18779
mobinAlijani <mobin138596@gmail.com>
2025-06-12 22:10:55 +0330

    add variables

---
+++ input_user/inputUser.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(void){
+    char answer[25];
+
+    std::cout << " please input your name : ";
+
+    std::cin >> answer;
+
+    std::cout << "hello " << answer <<"\n";
+
+    return 0;
+}
+
---
+++ variables/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+// integer number 
+
+int number = 23;
+
+//float(x); 
+
+float Fnumber=12.2;
+
+//or 
+
+double Dnumber =12.3;
+
+//char data type
+
+char grade ='a';
+
+//boolean (true , false)
+
+bool student = false;
+
+//string 
+
+std::string name = "mobin";
+
+// const 
+
+const double pi = 3.14159;
+
+
+int main(void){
+
+    std::cout << "hi " << name << "\n";
+
+
+    return 0;
+}
